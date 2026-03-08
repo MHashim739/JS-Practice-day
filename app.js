bed2d15750e7e5781b1a31980ea454fe7f3cf1c6
Muhammad hashim <mh4165209@gmail.com>
2026-03-07 16:52:11 -0800

    variable string and number & boolean variable paractice

---
+++ app.js
@@ -0,0 +1,20 @@
+// var a;
+// a = 1234;
+// console.log(typeof  a);
+
+// var num;
+// num = "have a bice day";
+// console.log(typeof num);
+
+
+// var a = 12345;
+// console.log( a);
+
+// var num = "have a nice day";
+// console.log( num);
+
+// var boo = false;
+// console.log( boo);
+
+
+
