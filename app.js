568c09db762a955f559fa2cf5a65fafa7a9c196c
Muhammad hashim <mh4165209@gmail.com>
2026-06-14 10:37:02 -0700

    before Js topic cover commit Advanced js Start

--- app.js
+++ app.js
@@ -259,12 +259,197 @@
 //     j++
 // }
 
-var userName;
-function userData(){
-    userName=prompt("enter your name");
-    console.log(userName);
+// var userName;
+// function userData(){
+//     userName=prompt("enter your name");
+//     console.log(userName);
     
-}
+// }
+
+                               // JavaScript Alert Parctice
+
+// alert("Hello Worlds !")
+
+                                // Javascript Data Type
+
+//Number
+1234
+//String
+//  text "123qww"//this not Numer this . string
+
+ //boolean
+
+ true & false;
+
+                                //varaiales in java Script
+
+var a=typeof(30);//the data  type Number
+
+var b=typeof("1234"); //the data type string
+var c=typeof("hello Worlds");  //& the data type in  String
+
+var d=typeof(true);//the data type boolen
+var e=typeof(false);//& the data type in  boolean
+
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+                 //variable name underscore & camelcase
+                 //var this_is_an_apple //underscore case
+                 //var thisIsAnApple //camelcase
+
+
+                       //javascript math operetor
+                       
+// var num1=2;
+// var num2=84;
+// var result=num1 & num2;
+// alert(result)
+
+                 //incerment & decrement Operator
+
+// var a=9;        
+// a++;
+// ++a;
+// --a;
+
+// alert(a)         
+
+// ++a;
+// --a;
+// a--;
+// alert(a)         
+
+
+
+
+// var student_01={
+//       name:"kaif",
+//       id:12,
+//       class:10,
+//       course:"Web & App",
+//       greet:function(){
+//             console.log("Hello From" + this.name);
+            
+//       }
+// }
+// console.log(student_01.name);
+// console.log(student_01.name + student_01.id + student_01.course);
+// student_01.name="ahsan";
+// student_01.course="Agentic Ai ";
+// console.log(student_01.course);
+// function CreateObj(name,id,age,course){
+//       this.name=name;
+//       this.id=id;
+//       this.age=age;
+//       this.course=course;
+// }
+// var objCreate= new CreateObj("Ahsan", 5 , 16+"years" , "Web & App");
+// console.log(objCreate);
+// var practiceObj=[{name:"Ahsan",id:12, DOF:"12-sep-1999"} ,{name:"Kaif",id:3,DOF:"3-jun-2002"}]
+// var popObj=practiceObj.pop();
+// var pushObj=practiceObj.push({name:"New Student",id:77607,})
+// console.log(practiceObj);
+// console.log(popObj);
+// console.log(pushObj);
+// for(var i=0; i<practiceObj.length; i++){
+// console.log(practiceObj[i].name);
+
+// }
+
+// function CreateObj(name,id){
+//       this.name=name;
+//       this.id=id;
+//       cube=function(a,b){
+//             var cube=a ** b;
+//             return a , b
+
+//       }
+// }
+
+// var test_01=new CreateObj("bilal",12,cube(3,3));
+// var test_02=new CreateObj("Kaif",5, cube(5,5));
+// console.log(test_01);
+// console.log(test_02);
+
+// function User(name,id){
+//     var name1=  this.name=name;
+//      var name2= this.id=id;
+//      contructor1=function(a){
+//       var cube= a -1 ;
+//       return cube;
+//      }
+// }
+// var saveUserData1=new User("Javed Ali",200);
+// var saveUserData2=new User("Ahmed Ali",202);
+// console.log(saveUserData1.name,contructor1(2));
+// console.log(saveUserData2);
+// var Save1=new  User < contructor1(11,12);
+// var save2= new User < contructor1(14,17);
+// console.log(save1);
+// console.log(save2);
+// function User1(name,id){
+//       this.name=name;
+//       this.id=id;
+// }
+// var user2={
+//       name:"Ahmed",
+//       id:206
+// }
+// var student_01=user2.name+user2.id;
+// var student_02=new User1("Ahmed Raza",2020);
+// console.log(student_01 instanceof(User1));
+// console.log(student_02 instanceof(User1));
+// function Contructor_01(name,id){
+//       this.name=name;
+//       this.id=id;
+
+// }
+// Contructor_01.prototype.self=function(){
+//       console.log("Good Morning " + this.name);
+      
+// }
+// var std_01=new Contructor_01("Ahsan Ali",77607);
+// var std_02=new Contructor_01("Mustafa Ali",7878);
+// var save_data1_prototype=std_01.self();
+// var save_data2_prototype=std_02.self();
+// console.log(std_01)
+// console.log(std_02);
+// console.log(save_data1_prototype);
+// console.log(save_data2_prototype);
+
+// this primitive value //copy by value
+
+
+// var a=10;   
+// var b=a;
+// b++;
+// console.log(a,b);
+
+// var a=false;
+// var b=a;
+// b=true;
+// console.log(a,b);
+
+// non primitive value
+
+// copy by refrence
+
+// var a=[1,2,3,4,5];
+// a[0]=4;
+// var b=a;
+// b[0]=[2];
+
+// console.log(a,b);
+ var a={name:"sameer"}
+ var b=a;
+ b.name="yahaz"
+ console.log(a);
+ console.log(b);
+ 
+ 
 
 
 
@@ -273,9 +458,5 @@ function userData(){
 
 
 
-
-
-
-
-
+ 
 
